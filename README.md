9e3df7e525b7dd9e7ba1ba222ea884602334031e
Youssef M. Abbas <beepmuffin@protonmail.com>
2025-07-08 22:28:15 +0300

    🎉 Where the journey starts.
    The base files for the repo a README and a small introduction threads code

---
+++ README.md
@@ -0,0 +1,13 @@
+# My C POSIX-Threading learning journey
+
+## 📚 Resources
+
+### 🍑 Bottom-G (Basics)
+
+- [Code Vault Youtube Course](https://www.youtube.com/playlist?list=PLfqABt5AS4FmuQf70psXrsMLEDQXNkLq2)
+- [W3 Multithreading in C with POSIX Threads: A Complete Guide](https://www.w3resource.com/c-programming/c-multithreading.php)
+
+### 💎 Top-G (Advanced)
+
+- [Programming with POSIX Threads - Book](https://www.amazon.com/Programming-POSIX-Threads-David-Butenhof/dp/0201633922)
+- [Introduction to Parallel Computing Tutorial - Academic Papers](https://hpc.llnl.gov/documentation/tutorials/introduction-parallel-computing-tutorial)
---
+++ intro_threads.c
@@ -0,0 +1,40 @@
+#include <asm-generic/errno-base.h>
+#include <errno.h>
+#include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+void *routine() {
+  printf("Hello from thread!\n");
+  sleep(1);
+  printf("Bye from thread!\n");
+  sleep(1);
+  return NULL;
+}
+
+int main(int argc, char *argv[]) {
+
+  pthread_t thread1, thread2;
+
+  if (pthread_create(&thread1, NULL, &routine, NULL) != 0) {
+    perror("pthread_create()");
+    exit(EXIT_FAILURE);
+  }
+  if (pthread_join(thread1, NULL) != 0) {
+    perror("pthread_join()");
+    exit(EXIT_FAILURE);
+  }
+
+  if (pthread_create(&thread2, NULL, &routine, NULL) != 0) {
+    perror("pthread_create()");
+    exit(EXIT_FAILURE);
+  }
+  if (pthread_join(thread2, NULL) != 0) {
+    perror("pthread_join()");
+    exit(EXIT_FAILURE);
+  }
+
+  return EXIT_SUCCESS;
+}
