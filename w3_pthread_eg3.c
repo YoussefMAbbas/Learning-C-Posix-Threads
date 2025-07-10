2dd5e7b41df30468e65601e5233f938f6d1777fe
Youssef M. Abbas <beepmuffin@protonmail.com>
2025-07-10 05:06:51 +0300

    ✨ W3Resource struct argument
    Using struct as an argument for the thread

---
+++ w3_pthread_eg3.c
@@ -0,0 +1,46 @@
+#include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+#define THREADS_NUMS 3
+
+void ErrMsg(const char *msg);
+void *UserInfo(void *args);
+
+typedef struct {
+  int age;
+  char firstName[30];
+} User;
+
+int main(int argc, char *argv[]) {
+
+  pthread_t thread;
+  User user = {.firstName = "Joe", .age = 20};
+
+  int threadCreated = pthread_create(&thread, NULL, UserInfo, (void *)&user);
+  if (threadCreated != 0) {
+    ErrMsg("pthread_create()");
+  }
+
+  int threadJoined = pthread_join(thread, NULL);
+  if (threadJoined != 0) {
+    ErrMsg("pthread_join()");
+  }
+
+  printf("Complete!\n");
+
+  return EXIT_SUCCESS;
+}
+
+void *UserInfo(void *args) {
+  User *user = (User *)args;
+  printf("First Name: %s\n", user->firstName);
+  printf("Age: %d\n", user->age);
+  pthread_exit(NULL);
+}
+
+void ErrMsg(const char *msg) {
+  perror(msg);
+  exit(EXIT_FAILURE);
+}
