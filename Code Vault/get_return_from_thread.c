5faa8a9117abfac78838ab2e341c91d7f4eaa6c2
Youssef M. Abbas <beepmuffin@protonmail.com>
2025-07-10 23:42:51 +0300

    ✨ Returning a value from a routine
    Getting a value from a procedure (void) like it's a function (return function) using dynamic allocation

---
+++ Code Vault/get_return_from_thread.c
@@ -0,0 +1,46 @@
+#include <pthread.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <unistd.h>
+
+void ErrMsg(const char *msg);
+void *Routine();
+
+int main(int argc, char *argv[]) {
+
+  srand((unsigned int)time(NULL));
+  pthread_t thread;
+  int *res;
+
+  int threadCreated = pthread_create(&thread, NULL, &Routine, NULL);
+  if (threadCreated != 0) {
+    ErrMsg("pthread_create()");
+  }
+
+  int threadJoined = pthread_join(thread, (void **)&res);
+  if (threadJoined != 0) {
+    ErrMsg("pthread_join()");
+  }
+
+  printf("MRes addr: %p\n", (void *)res);
+  printf("MRes value: %d\n", *res);
+  free(res);
+
+  return EXIT_SUCCESS;
+}
+
+void *Routine() {
+  int num = (rand() % 6) + 1;
+  int *res = malloc(sizeof(int));
+  *res = num;
+  printf("RRes addr: %p\n", (void *)res);
+  printf("RRes value: %d\n", *res);
+  return (void *)res;
+}
+
+void ErrMsg(const char *msg) {
+  perror(msg);
+  exit(EXIT_FAILURE);
+}
